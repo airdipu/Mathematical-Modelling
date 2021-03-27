629a6fd18735c05cc149e0017e5a0eeb7e6ecf7e
airdipu <airdipu@gmail.com>
2021-03-27 23:13:39 +0600

    falling ball angle

---
+++ Ball_angle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 27 23:10:39 2021
+
+@author: arahman
+"""
+
+from math import *
+
+x = 10             # Horizontal position
+y = 10             # Vertical position
+angle = atan(y/x)
+print ((angle/pi)*180)
