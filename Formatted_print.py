b9187807e98119e3e44bdc46345e9d473ec1d222
airdipu <airdipu@gmail.com>
2021-03-28 23:47:37 +0600

    formatted print out

---
+++ Formatted_print.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 28 22:38:15 2021
+
+@author: arahman
+"""
+
+real = 12.89643
+integer = 42
+string = 'some message'
+print ('real=%.3f, integer=%d, string=%s' % (real, integer, string))
+print ('real=%9.3e, integer=%5d, string=%s' % (real, integer, string))
+ 
